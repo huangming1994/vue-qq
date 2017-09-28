202140601fdc9b06b3e359ad5fd274a4bf8f3906
huangming1994 <1157114498@qq.com>
2017-09-28 15:32:09 +0800

    eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,3 @@
-// http://eslint.org/docs/user-guide/configuring
-
 module.exports = {
   root: true,
   parser: 'babel-eslint',
@@ -17,11 +15,22 @@ module.exports = {
   ],
   // add your custom rules here
   'rules': {
-    // allow paren-less arrow functions
-    'arrow-parens': 0,
-    // allow async-await
-    'generator-star-spacing': 0,
-    // allow debugger during development
+    "indent": [
+      "error",
+      2
+    ],
+    "linebreak-style": [
+      "error",
+      "unix"
+    ],
+    "quotes": [
+      "error",
+      "single"
+    ],
+    "semi": [
+      "error",
+      "never"
+    ],
     'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
   }
 }
